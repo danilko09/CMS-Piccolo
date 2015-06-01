0c2cefe9a2090cab757107e14b4058146c3774ca
danilko09 <kolesnikov.da0@gmail.com>
2015-06-01 09:30:03 +0400

    Добавлена поддержка хлебных крошек
    Добавлена полная поддержка хлебных крошек и в шаблон теперь ещё
    передается и id новости.

--- package/scripts/simple_blog.php
+++ package/scripts/simple_blog.php
@@ -10,6 +10,20 @@ class piccolo_blog{
         if(self::$base === null){self::$base = PICCOLO_ENGINE::loadConfig('piccolo_blog');}//Грузим конфиг
     }
     
+    //хук из page_data
+    public static function bindPage(){
+        $uri = PICCOLO_ENGINE::getURI();
+        $post_id = array_shift($uri);
+        if(PICCOLO_ENGINE::checkScript('breadcrumbs')){
+            breadcrumbs::setTitle('/' . rtrim(implode('/', $uri), '/'),PICCOLO_ENGINE::translate('BLOG_TITLE','piccolo_blog'));
+        }
+        if(is_numeric($post_id) && isset(self::$base['posts'][$post_id])){
+            return array('title'=>PICCOLO_ENGINE::translate('BLOG_TITLE','piccolo_blog'),'content'=>self::showPost($post_id));
+        }else{
+            return array('title'=>PICCOLO_ENGINE::translate('BLOG_TITLE','piccolo_blog'),'content'=>self::showPage());
+        }
+    }
+    
     //Отображение страницы
     public static function showPage(){
         
@@ -31,14 +45,26 @@ class piccolo_blog{
      
         $c = 0;
         
-        foreach(self::$base['posts'] as $post){
+        foreach(self::$base['posts'] as $id => $post){
             if(isset(self::$base['onpage']) && self::$base['onpage'] < $c){break;}
             $c++;
-            $posts .= PICCOLO_ENGINE::getRTmpl('piccolo_blog/blog_post', $post);
+            $posts = PICCOLO_ENGINE::getRTmpl('piccolo_blog/blog_post', $post + array('id'=>$id)).$posts;
         }
         
         return $posts;
         
+    }
+    
+    private static function showPost($id){
+        if(!isset(self::$base['posts'][$id])){return PICCOLO_ENGINE::translate('BAD_POST_ID','piccolo_blog');}
+        $post = self::$base['posts'][$id];
+        $post['id'] = $id;
+        if(!isset($post['content_full']) || $post['content_full'] == '' || $post['content_full'] == null){$post['content_full'] = $post['content'];}
+        $uri = PICCOLO_ENGINE::getURI();
+        if(PICCOLO_ENGINE::checkScript('breadcrumbs')){
+            breadcrumbs::setTitle('/' . rtrim(implode('/', $uri), '/'),$post['title']);
+        }
+        return PICCOLO_ENGINE::getRTmpl('piccolo_blog/post_page', $post);
     }
     
 }
