9994195d33c1d7a2cd16dc5607ec9d540915d600
danilko09 <kolesnikov.da0@gmail.com>
2015-07-15 20:05:09 +0400

    Добавлена проверка формы
    Просто добавил валидацию (проверка только на null), куда же без неё в
    наши времена ?

--- packages/piccolo_blog_admin/piccolo_blog_admin.php
+++ packages/piccolo_blog_admin/piccolo_blog_admin.php
@@ -5,16 +5,17 @@
         public static function bindAdminPage(){
             $msg = '';
             if(filter_input(INPUT_POST,'piccolo_blog_add_secret') !== null){
-                $blog_db = PICCOLO_ENGINE::loadConfig('piccolo_blog');
-                $blog_db['posts'][] = filter_input_array(INPUT_POST) 
-                        + array(
-                            'title'=>PICCOLO_ENGINE::translate('NO_TITLE','piccolo_blog_admin'),
-                            'content'=>PICCOLO_ENGINE::translate('NO_CONTENT','piccolo_blog_admin'),
-                            'author'=>PICCOLO_ENGINE::translate('NO_AUTHOR','piccolo_blog_admin'),
-                            'views'=>'0'
-                            );
-                PICCOLO_ENGINE::updateConfig('piccolo_blog',$blog_db);
-                $msg = PICCOLO_ENGINE::translate('SUCCESS','piccolo_blog_admin');
+                
+                if(filter_input(INPUT_POST,'title') == null){$msg .= PICCOLO_ENGINE::translate('NO_TITLE','piccolo_blog_admin').'<br/>';}
+                if(filter_input(INPUT_POST,'author') == null){$msg .= PICCOLO_ENGINE::translate('NO_AUTHOR','piccolo_blog_admin').'<br/>';}
+                if(filter_input(INPUT_POST,'content') == null){$msg .= PICCOLO_ENGINE::translate('NO_CONTENT','piccolo_blog_admin').'<br/>';}
+                
+                if($msg === ''){
+                    $blog_db = PICCOLO_ENGINE::loadConfig('piccolo_blog');
+                    $blog_db['posts'][] = filter_input_array(INPUT_POST);
+                    PICCOLO_ENGINE::updateConfig('piccolo_blog',$blog_db);
+                    $msg = PICCOLO_ENGINE::translate('SUCCESS','piccolo_blog_admin');
+                }
             }
             return PICCOLO_ENGINE::getRTmpl('piccolo_blog_admin/add_form',array('msg'=>$msg));
         }
