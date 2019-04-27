f78a9856c0d201c9b812fe2cd96bccdc38aad6fa
danilko09 <kolesnikov.da0@gmail.com>
2019-04-27 17:00:16 +0400

    [base/core] Ядро вынесено в отдельный пакет
    Сделано это, чтобы его можно было ставить и обновлять
    как любой друго пакет, ну и соответственно
    указывать в зависимостях.

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    if(is_file('install.php')){header('Location: install.php');}//Если есть установщик - переходим на него
-    include __DIR__ . DIRECTORY_SEPARATOR . 'piccolo.php'; //Загружаем ядро
-    PICCOLO_ENGINE::start(); //Запускаем генератор страницы
